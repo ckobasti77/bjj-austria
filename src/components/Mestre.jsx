1167ba0840b8008f39b66b1e5de8a8b2de214f38
Jovan Milojevic <jovanm028@gmail.com>
2022-12-28 15:02:01 +0100

    twenty second commit

--- src/components/Mestre.jsx
+++ src/components/Mestre.jsx
@@ -9,8 +9,8 @@ function Mestre() {
                 <div className="2xl:pl-24">
                     <div className="flex flex-col xl:flex-row gap-y-12 md:grid-cols-2 md:gap-x-8 2xl:gap-x-20">
                         <div className="mx-8 xl:mx-0 text-center xl:w-7/12 md:py-16 xl:py-24 md:text-left">
-                            <h1 className="leading-tight font-bold text-[#252525] dark:text-white text-4xl lg:text-5xl xl:text-6xl lg:leading-tight text-left" data-aos='fade-right' data-aos-delay='400'>Fernando Paulon </h1>
-                            <h2 className="max-w-2xl font-bold mt-4 text-2xl text-[#4d4d4d] dark:text-[#ccc] pb-3 text-left" data-aos='fade-right' data-aos-delay='500'>CHEFTRAINER</h2>
+                            <h1 className="leading-tight font-bold text-[#252525] dark:text-white text-4xl lg:text-5xl xl:text-6xl lg:leading-tight text-center xl:text-left" data-aos='fade-right' data-aos-delay='400'>Fernando Paulon </h1>
+                            <h2 className="max-w-2xl font-bold mt-4 text-2xl text-[#4d4d4d] dark:text-[#ccc] pb-3 text-center xl:text-left" data-aos='fade-right' data-aos-delay='500'>CHEFTRAINER</h2>
                             <blockquote>
                                 <p className="text-xl font-semibold leading-relaxed text-[#252525] dark:text-white text-center xl:text-left" data-aos='fade-right' data-aos-delay='600'>Fernando Paulon ist ein Schwarzgurt im 5. Grad im Brazilian Jiu-Jitsu unter der Ausbildung des legendären Mixed Martial Artists Murilo Bustamante.
                                     Im Alter von 16 Jahren schrieb sich Fernando Paulon an der Carlson Gracie School ein und trainierte im Anschluss im Brazilian Top Team,
